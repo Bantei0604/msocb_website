140f6e28c383b3636d6b69d26f7e28f13cc25a49
bantei <banteilangmyrthong06@gmail.com>
2025-03-19 14:02:34 +0530

    Updates

---
+++ ebc70433-3872a15f9b580ed1c52a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[509],{7094:function(t,a,c){c.d(a,{OId:function(){return r}});var e=c(3621);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=ebc70433-3872a15f9b580ed1c52a.js.map
